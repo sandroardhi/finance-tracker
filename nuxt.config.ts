36eaafd2e0909e68cd10027b380ac2795cc886ba
sandroardhi <sandrosaputra557@gmail.com>
2024-06-01 20:54:40 +0700

    typo fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     classSuffix: ''
   },
   runtimeConfig: {
-    pubic: {
+    public: {
       baseUrl: process.env.BASE_URL ?? 'http://localhost:3000'
     }
   }
